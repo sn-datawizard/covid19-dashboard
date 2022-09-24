07ca3b68c8c84efc2064625f2ef7b5db74d830c1
sn-datawizard <77932366+sn-datawizard@users.noreply.github.com>
2022-09-24 18:55:32 +0200

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -2,9 +2,9 @@ import pandas as pd
 import sqlalchemy
 import mysql.connector
 
-df = pd.read_csv('gs://covid-gcp-bucket1/data', sep=';', storage_options={'token': './sns-covid-gcp-b6475c6924f9.json'})
+df = pd.read_csv('gs://*', sep=';', storage_options={'token': './KEYFILE.json'}) #Sensitive data not visible
 #print(df.head())
 
-engine = sqlalchemy.create_engine('mysql+mysqlconnector://root:root@34.141.87.84/covid')
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://user:password@databaseip/covid') #Sensitive data not visible
 print(engine.connect)
 df.to_sql(name='table1', con=engine, if_exists = 'replace', index=False)
