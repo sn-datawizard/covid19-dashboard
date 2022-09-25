394de8b1dff0bc08055b830ea96eca79a3fd9af6
sn-datawizard <77932366+sn-datawizard@users.noreply.github.com>
2022-09-25 13:29:51 +0200

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -2,9 +2,9 @@ import pandas as pd
 import sqlalchemy
 import mysql.connector
 
-df = pd.read_csv('gs://*', sep=';', storage_options={'token': './KEYFILE.json'}) #Sensitive data not visible
+df = pd.read_csv('gs://<path>', sep=';', storage_options={'token': './KEYFILE.json'})
 #print(df.head())
 
-engine = sqlalchemy.create_engine('mysql+mysqlconnector://user:password@databaseip/covid') #Sensitive data not visible
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://<user>:<password>@<databaseip>/covid')
 print(engine.connect)
 df.to_sql(name='table1', con=engine, if_exists = 'replace', index=False)
